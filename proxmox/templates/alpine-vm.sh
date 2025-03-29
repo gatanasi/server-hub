ed59e6cba53a26043d6c68652505f3fbc6c8a54a
gatanasi <gatanasios@gmail.com>
2025-03-29 11:27:01 +0100

    Simplified template creation

--- proxmox/templates/alpine-vm.sh
+++ proxmox/templates/alpine-vm.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-qm create 2000 --memory 2048 --cores 2 --cpulimit 2 --name alpine --cpu x86-64-v4 --machine q35,viommu=intel --ostype l26 --agent enabled=1 --net0 virtio,bridge=vmbr0,firewall=1 --onboot 1 --tags alpine --template 1
-qm set 2000 --bios ovmf --efidisk0 local-zfs:4,efitype=4m,pre-enrolled-keys=1
-qm importdisk 2000 /var/lib/vz/template/iso/generic_alpine-3.21.2-x86_64-bios-cloudinit-r0.qcow2 local-zfs
-qm set 2000 --scsihw virtio-scsi-pci --scsi0 local-zfs:vm-2000-disk-1,discard=on,ssd=1
-qm set 2000 --ide2 local-zfs:cloudinit
-qm set 2000 --ciuser alpine
-qm set 2000 --ipconfig0 ip=dhcp
-qm set 2000 --boot c --bootdisk scsi0
-qm set 2000 --serial0 socket --vga serial0
-qm disk resize 2000 scsi0 4G
+VMID=2000
+IMAGE_PATH="/var/lib/vz/template/iso/generic_alpine-3.21.2-x86_64-bios-cloudinit-r0.qcow2"
+DISTRO="alpine"
+
+. ./create-vm.sh
---
+++ proxmox/templates/create-vm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+qm create "$VMID" --memory 4096 --cores 2 --cpulimit 2 --name "$DISTRO" --cpu x86-64-v4 --machine q35,viommu=intel --ostype l26 --agent enabled=1 --net0 virtio,bridge=vmbr0,firewall=1 --onboot 1 --tags "$DISTRO" --template 1
+qm set "$VMID" --bios ovmf --efidisk0 local-zfs:1,efitype=4m,pre-enrolled-keys=1
+qm importdisk "$VMID" "$IMAGE_PATH" local-zfs
+qm set "$VMID" --scsihw virtio-scsi-pci --scsi0 local-zfs:vm-"$VMID"-disk-1,discard=on,ssd=1
+qm set "$VMID" --ide2 local-zfs:cloudinit
+qm set "$VMID" --ciuser "$DISTRO"
+qm set "$VMID" --ipconfig0 ip=dhcp
+qm set "$VMID" --boot c --bootdisk scsi0
+qm set "$VMID" --serial0 socket --vga serial0
+qm disk resize "$VMID" scsi0 4G
