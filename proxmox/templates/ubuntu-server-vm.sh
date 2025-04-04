c930a2a7ff47adedfdb8efbf2556eb19d2c26f3f
gatanasi <gatanasios@gmail.com>
2025-04-04 14:52:57 +0200

    Update Ubuntu VM image

--- proxmox/templates/ubuntu-server-vm.sh
+++ proxmox/templates/ubuntu-server-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DISTRO="ubuntu"
-IMAGE_PATH="/var/lib/vz/template/iso/oracular-server-cloudimg-amd64.img"
+IMAGE_PATH="/var/lib/vz/template/iso/noble-server-cloudimg-amd64.img"
 
 ./create-vm.sh "$1" "$DISTRO" "$IMAGE_PATH"
